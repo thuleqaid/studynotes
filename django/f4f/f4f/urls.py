07e560bc3de89f207c6179c63df4f63e73068b98
Tian Quan <quan_tian@dnss.denso.com.cn>
2013-02-19 17:32:28 +0800

    django_2

--- django/f4f/f4f/urls.py
+++ django/f4f/f4f/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'f4f.views.home', name='home'),
     # url(r'^f4f/', include('f4f.foo.urls')),
+    url(r'^qa/', include('questionnaire.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- django/f4f/questionnaire/views.py
+++ django/f4f/questionnaire/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from questionnaire.models import *
+
+def index(request):
+    titles=Title.objects.all()
+    return render_to_response('questionnaire/index.html',{'titles':titles})
+
+def doTest(request,titleid):
+    quiz={}
+    print(request)
+    if 'LASTQINDEX' in request.COOKIES:
+        question=Question.objects.get(title_id=titleid,qindex=int(request.COOKIES['LASTQINDEX'])+1)
+    else:
+        question=Question.objects.get(title_id=titleid,qindex=1)
+    qindex=question.qindex
+    choices=question.current.all()
+    request.COOKIES['LASTQINDEX']=qindex
+    rc=RequestContext(request,{'LASTQINDEX':qindex})
+    print(rc)
+    return render_to_response('questionnaire/quiz.html',{'question':question,'choices':choices,'titleid':titleid},context_instance=rc)
